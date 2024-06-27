ab90976f88db8fa0408ab2da06c030a646c7328c
Oleksii Shevchenko <peace_oleksij@proton.me>
2024-06-28 01:32:18 +0300

    Some changing with build system

--- buildbin.sh
+++ buildbin.sh
@@ -1,8 +1,8 @@
-npm run distlinux32
+pnpm run distlinux32
 mv dist/*.AppImage dist/bin/
-npm run distlinux64
+pnpm run distlinux64
 mv dist/*.AppImage dist/bin/
-npm run distwin32
+pnpm run distwin32
 mv dist/*.exe dist/bin/win32/
-npm run distwin64
+pnpm run distwin64
 mv dist/*exe dist/bin/
