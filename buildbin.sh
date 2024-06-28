f3ba600ef305b38bacc66f3803f4c7ed422069da
Oleksii Shevchenko <peace_oleksij@proton.me>
2024-06-28 03:44:30 +0300

    Remove old build sustem file

--- buildbin.sh
+++
@@ -1,8 +0,0 @@
-pnpm run distlinux32
-mv dist/*.AppImage dist/bin/
-pnpm run distlinux64
-mv dist/*.AppImage dist/bin/
-pnpm run distwin32
-mv dist/*.exe dist/bin/win32/
-pnpm run distwin64
-mv dist/*exe dist/bin/
